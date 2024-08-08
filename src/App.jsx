4f7f3ceb8c0d88de5d74d917d6a7c6e5eebdbd34
jatinkumar30 <bhanurawal.30@gmail.com>
2024-08-08 12:04:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+
+const App = () => {
+
+const [articles , setArticles] = useState([])
+
+  useEffect(()=>{ 
+    Axios.get("https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=80f2f2d0859d7a5b4d343c29cc9eb471").then((response)=>{
+        setArticles(response.data.articles)
+    })
+  },[]);
+
+  return (
+    <>
+    <h1 className='font-bold text-3xl text-center pt-8'>Global News</h1>
+    <div className='flex justify-center flex-wrap gap-4 p-8 '>
+      {articles.map((article,index)=>(
+        <div key={index} className='bg-gray-200 w-[32rem] rounded-lg'>
+        <h1 className='font-semibold text-lg p-4'>{article.title}</h1>
+        <img className='p-6' src={article.image} alt="Not rendered" />
+        <p className='text-base p-4 '> {article.description}</p>
+        <a href={article.url} className='ml-4 pb-3'>Read More ...</a>
+      </div>
+      ))}
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
